66e2ad69aa3cb0f28c7113d8e698e9e715faa31d
Kelvin Mukotso <kmukotso@Cytonns-MacBook-Pro.local>
2023-04-02 10:54:22 +0300

    remove default failed jobs and personal access tokens table and add index to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('uuid',36);
-            $table->string('first_name',255);
-            $table->string('last_name',255);
+            $table->string('first_name',255)->index();
+            $table->string('last_name',255)->index();
             $table->boolean('is_admin')->default('false');
-            $table->string('email',255)->unique();
+            $table->string('email',255)->unique()->index();
             $table->timestamp('email_verified_at');
             $table->string('password',255);
-            $table->string('avatar',36);
+            $table->string('avatar',36)->nullable();
             $table->string('address',255);
             $table->string('phone_number',255);
             $table->boolean('is_marketing')->default('false');
