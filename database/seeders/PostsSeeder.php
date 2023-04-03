e60aa875af482ab3a3af4e2aa4659835e0566d84
mukotso <kelvinmukotso@gmail.com>
2023-04-03 22:57:35 +0300

    create categories seeders

--- database/seeders/PostsSeeder.php
+++ database/seeders/PostsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class postsSeeder extends Seeder
+class PostsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
