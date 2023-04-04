7ba032a5b53bd24cf705141a3c82438df014ff95
Mukotso <kelvinmukotso@gmail.com>
2023-04-04 07:26:26 +0300

    add HasUuids to all models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Category extends Model
 {
-    use HasFactory;
-}
+    use HasFactory, HasUuids;
+  
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Payment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+  
 }
