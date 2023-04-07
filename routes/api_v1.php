e45dd2b5594ff6e77372f84d88ffeb1f743efaa8
mukotso <kelvinmukotso@gmail.com>
2023-04-07 23:15:30 +0300

    create and register has_user_access and has_admin_access middleware and use them to protect routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -28,54 +28,59 @@ Route::post('user/login', [ApiAuthController::class, 'login']);
 Route::get('user/forgot-password', [ApiAuthController::class, 'forgotPassword']);
 Route::post('user/reset-password-token', [ApiAuthController::class, 'resetPassword']);
 
+
+Route::middleware('auth', 'has_admin_access')->group(function () {
 //admin routes
-Route::prefix('admin')->group(function () {
-    Route::get('create', [UsersController::class, 'store']);
-    Route::get('logout', [ApiAuthController::class, 'logout']);
-    Route::get('user-listing', [UsersController::class, 'index']);
-    Route::get('user-edit/{uuid}', [UsersController::class, 'index']);
-    Route::get('user-delete/{uuid}', [UsersController::class, 'destroy']);
+    Route::prefix('admin')->group(function () {
+        Route::get('create', [UsersController::class, 'store']);
+        Route::get('logout', [ApiAuthController::class, 'logout']);
+        Route::get('user-listing', [UsersController::class, 'index']);
+        Route::get('user-edit/{uuid}', [UsersController::class, 'index']);
+        Route::get('user-delete/{uuid}', [UsersController::class, 'destroy']);
+    });
 });
 
+Route::middleware('auth', 'has_user_access')->group(function () {
 //user end points
-Route::get('user/{uuid}', [UsersController::class, 'show']);
-Route::delete('user/{uuid}', [UsersController::class, 'destroy']);
-Route::get('user/{uuid}/orders', [OrdersController::class, 'getUserOrders']);
-Route::get('user/create', [UsersController::class, 'store']);
-Route::get('user/login', [ApiAuthController::class, 'logout']);
-Route::put('user/edit', [UsersController::class, 'update']);
+    Route::get('user/{uuid}', [UsersController::class, 'show']);
+    Route::delete('user/{uuid}', [UsersController::class, 'destroy']);
+    Route::get('user/{uuid}/orders', [OrdersController::class, 'getUserOrders']);
+    Route::get('user/create', [UsersController::class, 'store']);
+    Route::get('user/login', [ApiAuthController::class, 'logout']);
+    Route::put('user/edit', [UsersController::class, 'update']);
 
 //main page Routes
-Route::prefix('main')->group(function () {
-    Route::resource('blog', PostsController::class);
-    Route::resource('promotions', PromotionsController::class);
-});
+    Route::prefix('main')->group(function () {
+        Route::resource('blog', PostsController::class);
+        Route::resource('promotions', PromotionsController::class);
+    });
 
 //brand Routes
-Route::resource('brands', BrandsController::class);
+    Route::resource('brands', BrandsController::class);
 
 
 //categories Routes
-Route::resource('categories', CategoriesController::class);
+    Route::resource('categories', CategoriesController::class);
 
 
 //orders Routes
-Route::resource('orders', OrdersController::class);
-Route::get('/order/{uuid}/download', [OrdersController::class, 'download']);
-Route::get('/orders/dashboard', [OrdersController::class, 'dashboardOrders']);
-Route::get('/orders/shipment-locator', [OrdersController::class, 'shipmentLocator']);
+    Route::resource('orders', OrdersController::class);
+    Route::get('/order/{uuid}/download', [OrdersController::class, 'download']);
+    Route::get('/orders/dashboard', [OrdersController::class, 'dashboardOrders']);
+    Route::get('/orders/shipment-locator', [OrdersController::class, 'shipmentLocator']);
 
 //order statuses routes
-Route::resource('order-statuses', OrderStatusesController::class);
+    Route::resource('order-statuses', OrderStatusesController::class);
 
 
 //payments  routes
-Route::resource('order-statuses', OrderStatusesController::class);
+    Route::resource('order-statuses', OrderStatusesController::class);
 
 //products  routes
-Route::resource('products', OrderStatusesController::class);
+    Route::resource('products', OrderStatusesController::class);
 
 
 //files  routes
-Route::get('/file/upload', [FilesController::class, 'upload']);
-Route::get('/file/{uuid}', [FilesController::class, 'show']);
+    Route::get('/file/upload', [FilesController::class, 'upload']);
+    Route::get('/file/{uuid}', [FilesController::class, 'show']);
+});
