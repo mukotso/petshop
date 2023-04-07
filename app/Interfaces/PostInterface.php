a787035e928c8c778957c325248e37048920f4f4
mukotso <kelvinmukotso@gmail.com>
2023-04-07 11:58:33 +0300

    remove CRUD operationf for posts and promotions interface and repository

--- app/Interfaces/PostInterface.php
+++ app/Interfaces/PostInterface.php
@@ -8,13 +8,8 @@ interface PostInterface
 
     public function getAllPosts();
 
-    public function createPost(array $post_details);
-
     public function showPostDetails($post_uuid);
 
-    public function updatePostDetails($post_uuid);
-
-    public function deletePost($post_uuid);
 
 
 }
--- app/Interfaces/PromotionInterface.php
+++ app/Interfaces/PromotionInterface.php
@@ -8,13 +8,7 @@ interface PromotionInterface
 
     public function getAllPromotions();
 
-    public function createPromotion(array $promotion_details);
-
     public function showPromotionDetails($promotion_uuid);
-
-    public function updatePromotionDetails($promotion_uuid);
-
-    public function deletePromotion($promotion_uuid);
 
 
 }
