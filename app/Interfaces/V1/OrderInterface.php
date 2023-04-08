96879ddc4d164a720f2302d7f85bc0bf188c5983
mukotso <kelvinmukotso@gmail.com>
2023-04-08 12:19:02 +0300

    create CRUD apis for payment ,update order interface and repository

--- app/Interfaces/V1/OrderInterface.php
+++ app/Interfaces/V1/OrderInterface.php
@@ -10,7 +10,14 @@ interface OrderInterface
 
     public function createOrder(array $order_details);
 
+    public function showOrderDetails($order_uuid);
+    public function updateOrderDetails(array $order_details, $order_uuid);
+
     public function getUserOrders($user_uuid);
 
+    public function getShimentLocators();
 
+    public function downloadOrder($uuid);
+
+    public function dashboardOrderDetails();
 }
