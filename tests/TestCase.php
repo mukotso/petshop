5b1b3e716b7a77513ef1405cdc356456f0d9fa54
mukotso <kelvinmukotso@gmail.com>
2023-04-10 22:00:17 +0300

    add ProductFeature Test and give routes name

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,44 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Throwable;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
+
+    public User|null $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate:fresh --seed');
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function authenticateAdmin(): void
+    {
+        /** @var Authenticatable $user */
+        $user = $this->user = User::query()
+            ->where('is_admin', true)
+            ->first();
+        $this->actingAs($user, 'jwt');
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function authenticateUser(): void
+    {
+        /** @var Authenticatable $user */
+        $user = $this->user = User::query()->first();
+        $this->actingAs($user, 'jwt');
+    }
 }
