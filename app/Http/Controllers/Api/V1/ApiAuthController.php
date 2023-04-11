782e97b9cffd9d3772d8c6c5228dd40a083887aa
mukotso <kelvinmukotso@gmail.com>
2023-04-11 15:05:13 +0300

    install l5-swagger and setup configurations

--- app/Http/Controllers/Api/V1/ApiAuthController.php
+++ app/Http/Controllers/Api/V1/ApiAuthController.php
@@ -12,6 +12,50 @@ use Request;
 
 class ApiAuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     * path="/admin/login",
+     * summary="Sign in a user",
+     * description="Login by email, password",
+     * operationId="authLogin",
+     * tags={"auth"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass user credentials",
+     *    @OA\JsonContent(
+     *       required={"email","password"},
+     *       @OA\Property(property="email", type="string", format="email", example="admin@buckhill.co.uk"),
+     *       @OA\Property(property="password", type="string", format="password", example="admin"),
+     *    ),
+     * ),
+     *
+    @OA\Response(
+     * response=200,
+     * description="Successfull (Ok)",
+     * ),
+     * @OA\Response(
+     * response=403,
+     * description="Forbidden"
+     * ),
+     *
+     * @OA\Response(
+     * response=401,
+     * description="Unauthenticated"
+     * ),
+     * @OA\Response(
+     * response=404,
+     * description="Page not found"
+     * ),
+     *  @OA\Response(
+     * response=422,
+     * description="Unprocessable Entity"
+     * ),
+     *  @OA\Response(
+     * response=500,
+     * description="Internal server error"
+     * ),
+     * )
+     */
 
     public function login(UserLoginRequest $request)
     {
